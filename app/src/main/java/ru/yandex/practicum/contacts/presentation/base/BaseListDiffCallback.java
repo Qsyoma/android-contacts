62d1ec223d59bd63cc5c0f4d8e827af82b4c9a04
Qsyoma <kolesnikov.yk@gmail.com>
2025-01-11 22:42:38 +0300

    исправления

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,7 +1,10 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
+
+import ru.yandex.practicum.contacts.presentation.sort.SortTypeUI;
 
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T>{
 
@@ -10,14 +13,19 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
         ListDiffInterface n = new ListDiffInterface() {
             @Override
             public boolean theSameAs(Object o) {
-                return false;
+                return this.hashCode() == o.hashCode();
             }
         };
-        return false;
+        return oldItem.theSameAs(newItem);
     }
 
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return false;
+        return  oldItem.equals(newItem);
+    }
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+        return newItem;
     }
 }
